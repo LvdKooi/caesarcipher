d33cccd8cc0335e0fc09003e3481f9de760ddd80
lvdkooi <lvvdkooi@hotmail.com>
2024-06-16 16:04:28 +0200

    Initial commit

---
+++ src/test/java/nl/kooi/caesarcipher/CaesarcipherApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kooi.caesarcipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesarcipherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
