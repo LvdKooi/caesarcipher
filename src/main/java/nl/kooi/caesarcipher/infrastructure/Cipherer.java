104b7c407301bece5ad3fc3a27d5f3fb89dee7f5
lvdkooi <lvvdkooi@hotmail.com>
2024-07-02 21:26:03 +0200

    clean implementation of Cipherer

--- src/main/java/nl/kooi/caesarcipher/infrastructure/Cipherer.java
+++ src/main/java/nl/kooi/caesarcipher/infrastructure/Cipherer.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
@@ -13,15 +14,25 @@ import java.util.stream.IntStream;
 @Service
 public class Cipherer implements CipheringService {
 
+    private static final Map<Integer, String> ALPHABET = getAlphabet(26);
 
     @Override
     public String cipher(String input, int offset) {
-        var alphabet = getAlphabet(26);
-        var cipheredAlphabet = getAlphabet(offset);
-
         return Arrays.stream(input.split(""))
-                .map(letter -> alphabet.entrySet().stream().filter(entry -> entry.getValue().equals(letter)).map(Map.Entry::getKey).map(cipheredAlphabet::get).peek(System.out::println).findAny().orElse(letter))
+                .map(mapToOffsetLetter(getAlphabet(offset)))
                 .collect(Collectors.joining());
+    }
+
+    private static UnaryOperator<String> mapToOffsetLetter(Map<Integer, String> toAlphabet) {
+        return letter ->
+                ALPHABET
+                        .entrySet()
+                        .stream()
+                        .filter(entry -> entry.getValue().equals(letter))
+                        .map(Map.Entry::getKey)
+                        .map(toAlphabet::get)
+                        .findAny()
+                        .orElse(letter);
     }
 
     private static Map<Integer, String> getAlphabet(int offset) {
@@ -29,16 +40,13 @@ public class Cipherer implements CipheringService {
 
         return IntStream.rangeClosed(1, 26)
                 .boxed()
-                .collect(Collectors.toMap(Function.identity(), i -> alphabetSet.get(calculateOffset(offset).apply(i) -1)));
+                .collect(Collectors.toMap(Function.identity(), i -> alphabetSet.get(calculateOffset(i, offset) - 1)));
     }
 
-    private static UnaryOperator<Integer> calculateOffset(int offset) {
-        return i -> {
-            var result = i + offset;
-            if (result > 26) {
-                return result - 26;
-            }
-            return result;
-        };
+    private static int calculateOffset(int i, int offset) {
+        return Optional.of(i + offset)
+                .filter(result -> result > 26)
+                .map(result -> result - 26)
+                .orElse(i + offset);
     }
 }
